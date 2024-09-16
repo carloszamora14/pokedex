fb78d6e2fa78569f3314fcc74880484475f5018d
Carlos Zamora <carlos14zamora@gmail.com>
2024-09-16 14:31:07 -0600

    add type icons and constants for their use and corresponding colors

---
+++ src/constants/colors.js
@@ -0,0 +1,22 @@
+const colors = {
+  bug: '#8D9B20',
+  dark: '#50443C',
+  dragon: '#8F84BB',
+  electric: '#D2A23C',
+  fairy: '#BF95BE',
+  fighting: '#7E5549',
+  fire: '#BD422A',
+  flying: '#959DC2',
+  ghost: '#959DC2',
+  grass: '#86B066',
+  ground: '#B7A574',
+  ice: '#71C4E0',
+  normal: '#DEDDDD',
+  poison: '#895B8A',
+  psychic: '#B6607D',
+  rock: '#9F915E',
+  steel: '#A3A3A9',
+  water: '#548DC3',
+};
+
+export default colors;
